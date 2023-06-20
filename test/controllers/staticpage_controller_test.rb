568d4a417ad149cca0a9bc39d30c64a6b3bade96
Angelo Geant Gaviola <43493935+KiDxS@users.noreply.github.com>
2023-06-20 13:34:50 +0800

    changed to root_path

--- test/controllers/staticpage_controller_test.rb
+++ test/controllers/staticpage_controller_test.rb
@@ -2,17 +2,16 @@ require "test_helper"
 
 class StaticpageControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
-  
-  test "should get index if user has paid" do
-    @user = users(:two)
-    sign_in @user
-    get staticpage_index_url
-    assert_response :success
-  end
   test "should redirect to the purchase access page if user hasn't paid yet" do
     @user = users(:one)
     sign_in @user
-    get staticpage_index_url
+    get root_path
     assert_redirected_to access_page_path
+  end
+  test 'should get index if user has paid' do
+    @user = users(:two)
+    sign_in @user
+    get root_path
+    assert_response :success
   end
 end
