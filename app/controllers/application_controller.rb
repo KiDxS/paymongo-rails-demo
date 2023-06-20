8f4f73f5d854693d52849754dc476faa39af9aed
Angelo Geant Gaviola <43493935+KiDxS@users.noreply.github.com>
2023-06-20 10:40:27 +0800

    created helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
-  def user_paid_yet?
+  before_action :authenticate_user!, only: [:index]
+  def dont_allow_paid_users_to_pay_again
     if current_user.paid == true
-      true
-    else
-      false
+      redirect_to root_path
     end
+  end
+  def if_user_has_not_paid_yet
+    redirect_to access_page_path unless current_user.paid == true
+    
   end
 end
